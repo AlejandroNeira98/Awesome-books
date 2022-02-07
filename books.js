97919dc221e0667b19e4f827481cddf5fe98fff2
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-07 14:12:49 -0500

    Linters

--- books.js
+++ books.js
@@ -2,24 +2,38 @@ const titleInput = document.getElementById('title-input');
 const authorInput = document.getElementById('author-input');
 const submitButton = document.getElementById('submit-button');
 let books = [];
-var removeBtns = document.querySelectorAll(".remove-btn");
 
-window.addEventListener('load', () => {
-  if (JSON.parse(localStorage.getItem('books')) !== null){
-    books = JSON.parse(localStorage.getItem('books'));
-    books.forEach(element => {
-      addBookToDocument(element);
-    })
-  }
+function removeBook(book) {
+  books = books.filter((element) => element.title !== book.title);
+  localStorage.setItem('books', JSON.stringify(books));
+}
 
-  submitButton.addEventListener('click', function(e){
-    e.preventDefault();
-    let author = authorInput.value;
-    let title = titleInput.value;
-    let book = {'title': title, 'author': author}
-    addBookToLocalStorage(book);
-  })
-});
+function removeBtnEventListener(element, title, author) {
+  const book = { title, author };
+  removeBook(book);
+  element.parentElement.remove();
+}
+
+function addBookToDocument(book) {
+  const booksDiv = document.getElementById('books');
+  const bookDiv = document.createElement('div');
+  const title = document.createElement('p');
+  title.classList.add('title');
+  const author = document.createElement('p');
+  author.classList.add('author');
+  const remove = document.createElement('button');
+  remove.classList.add('remove-btn');
+
+  title.innerText = book.title;
+  author.innerText = book.author;
+  remove.innerText = 'Remove';
+
+  bookDiv.append(title, author, remove);
+  booksDiv.append(bookDiv);
+  remove.addEventListener('click', () => {
+    removeBtnEventListener(remove, book.title, book.author);
+  });
+}
 
 function addBookToLocalStorage(book) {
   books.push(book);
@@ -27,34 +41,19 @@ function addBookToLocalStorage(book) {
   addBookToDocument(book);
 }
 
-function addBookToDocument(book){
-  let booksDiv = document.getElementById('books');
-  let bookDiv = document.createElement('div');
-  let title = document.createElement('p');
-  title.classList.add("title")
-  let author = document.createElement('p');
-  author.classList.add("author")
-  let remove = document.createElement('button');
-  remove.classList.add("remove-btn");
+window.addEventListener('load', () => {
+  if (JSON.parse(localStorage.getItem('books')) !== null) {
+    books = JSON.parse(localStorage.getItem('books'));
+    books.forEach((element) => {
+      addBookToDocument(element);
+    });
+  }
 
-  title.innerText = book.title;
-  author.innerText = book.author;
-  remove.innerText = "Remove";
-
-  bookDiv.append(title, author, remove);
-  booksDiv.append(bookDiv);   
-  remove.addEventListener('click', function (e) {
-    removeBtnEventListener(remove, book.title, book.author);
-  }); 
-}
-
-function removeBook(book){
-  books = books.filter(element => element.title !== book.title);
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-function removeBtnEventListener(element, title, author) {
-  let book = {'title': title, 'author': author};
-  removeBook(book);
-  element.parentElement.remove();
-}
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const author = authorInput.value;
+    const title = titleInput.value;
+    const book = { title, author };
+    addBookToLocalStorage(book);
+  });
+});
