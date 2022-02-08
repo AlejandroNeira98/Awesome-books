6c5db6f2390cfd97dc7c61fc47eaebccca044b9d
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-08 09:52:26 -0500

    Add Methods Class

--- books.js
+++ books.js
@@ -1,51 +1,58 @@
 const titleInput = document.getElementById('title-input');
 const authorInput = document.getElementById('author-input');
 const submitButton = document.getElementById('submit-button');
-let books = [];
 
-function removeBook(book) {
-  books = books.filter((element) => JSON.stringify(element) !== JSON.stringify(book));
-  localStorage.setItem('books', JSON.stringify(books));
+class methods {
+  constructor() {
+
+    this.removeBook = function (book) {
+      books = books.filter((element) => JSON.stringify(element) !== JSON.stringify(book));
+      localStorage.setItem('books', JSON.stringify(books));
+    };
+
+    this.removeBtnEventListener = function (element, title, author) {
+      const book = { title, author };
+      this.removeBook(book);
+      element.parentElement.remove();
+    };
+
+    this.addBookToDocument = function (book) {
+      const booksDiv = document.getElementById('books');
+      const bookDiv = document.createElement('div');
+      const title = document.createElement('p');
+      title.classList.add('title');
+      const author = document.createElement('p');
+      author.classList.add('author');
+      const remove = document.createElement('button');
+      remove.classList.add('remove-btn');
+
+      title.innerText = book.title;
+      author.innerText = book.author;
+      remove.innerText = 'Remove';
+
+      bookDiv.append(title, author, remove);
+      booksDiv.append(bookDiv);
+      remove.addEventListener('click', () => {
+        this.removeBtnEventListener(remove, book.title, book.author);
+      });
+    };
+
+    this.addBookToLocalStorage = function (book) {
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+      this.addBookToDocument(book);
+    };
+
+  }
 }
 
-function removeBtnEventListener(element, title, author) {
-  const book = { title, author };
-  removeBook(book);
-  element.parentElement.remove();
-}
-
-function addBookToDocument(book) {
-  const booksDiv = document.getElementById('books');
-  const bookDiv = document.createElement('div');
-  const title = document.createElement('p');
-  title.classList.add('title');
-  const author = document.createElement('p');
-  author.classList.add('author');
-  const remove = document.createElement('button');
-  remove.classList.add('remove-btn');
-
-  title.innerText = book.title;
-  author.innerText = book.author;
-  remove.innerText = 'Remove';
-
-  bookDiv.append(title, author, remove);
-  booksDiv.append(bookDiv);
-  remove.addEventListener('click', () => {
-    removeBtnEventListener(remove, book.title, book.author);
-  });
-}
-
-function addBookToLocalStorage(book) {
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-  addBookToDocument(book);
-}
+const method = new methods;
 
 window.addEventListener('load', () => {
   if (JSON.parse(localStorage.getItem('books')) !== null) {
     books = JSON.parse(localStorage.getItem('books'));
     books.forEach((element) => {
-      addBookToDocument(element);
+      method.addBookToDocument(element);
     });
   }
 
@@ -54,8 +61,8 @@ window.addEventListener('load', () => {
     const author = authorInput.value;
     const title = titleInput.value;
     const book = { title, author };
-    addBookToLocalStorage(book);
+    method.addBookToLocalStorage(book);
     authorInput.value = null;
     titleInput.value = null;
   });
-});
+ });
