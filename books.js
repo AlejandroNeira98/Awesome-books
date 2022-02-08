d80a9c0d17547724d72ce0be1b609eeaefb0390c
Saadat123456 <saadatlucky313@gmail.com>
2022-02-09 02:38:01 +0500

    Move Functions From Constructor To Method Class

--- books.js
+++ books.js
@@ -5,73 +5,67 @@ const submitButton = document.getElementById('submit-button');
 class Methods {
   constructor() {
     this.books = [];
-
-    this.init = function () {
-      if (JSON.parse(localStorage.getItem('books')) !== null) {
-        this.books = JSON.parse(localStorage.getItem('books'));
-        this.books.forEach((element) => {
-          this.addBookToDocument(element);
-        });
-      }
-    };
-
-    this.removeBook = function (time) {
-      this.books = this.books.filter((book) => book.time !== time);
-      localStorage.setItem('books', JSON.stringify(this.books));
-    };
-
-    this.removeBtnEventListener = function (element, time) {
-      this.removeBook(time);
-      element.parentElement.remove();
-    };
-
-    this.addBookToDocument = function (book) {
-      const booksDiv = document.getElementById('books');
-
-      const bookDiv = document.createElement('div');
-      bookDiv.setAttribute('data-time', book.time);
-
-      const bookDesc = document.createElement('p');
-      bookDesc.classList.add('book-description');
-      // title.classList.add('title');
-      // const author = document.createElement('p');
-      // author.classList.add('author');
-      const remove = document.createElement('button');
-      remove.classList.add('remove-btn');
-
-      // title.innerText = book.title;
-      // author.innerText = book.author;
-      bookDesc.innerText = `"${book.title}" by ${book.author}`;
-      remove.innerText = 'Remove';
-
-      bookDiv.append(bookDesc, remove);
-      booksDiv.append(bookDiv);
-      remove.addEventListener('click', () => {
-        this.removeBtnEventListener(remove, book.time);
-      });
-    };
-
-    this.addBookToLocalStorage = function (title, author) {
-      const d = new Date();
-      const time = d.getTime();
-      this.books.push({
-        title,
-        author,
-        time,
-      });
-      localStorage.setItem('books', JSON.stringify(this.books));
-      this.addBookToDocument({
-        title,
-        author,
-        time,
-      });
-    };
   }
+
+  init = function () {
+    if (JSON.parse(localStorage.getItem('books')) !== null) {
+      this.books = JSON.parse(localStorage.getItem('books'));
+      this.books.forEach((element) => {
+        this.addBookToDocument(element);
+      });
+    }
+  };
+
+  removeBook = function (time) {
+    this.books = this.books.filter((book) => book.time !== time);
+    localStorage.setItem('books', JSON.stringify(this.books));
+  };
+
+  removeBtnEventListener = function (element, time) {
+    this.removeBook(time);
+    element.parentElement.remove();
+  };
+
+  addBookToDocument = function (book) {
+    const booksDiv = document.getElementById('books');
+
+    const bookDiv = document.createElement('div');
+    bookDiv.setAttribute('data-time', book.time);
+
+    const bookDesc = document.createElement('p');
+    bookDesc.classList.add('book-description');
+    const remove = document.createElement('button');
+    remove.classList.add('remove-btn');
+    bookDesc.innerText = `"${book.title}" by ${book.author}`;
+    remove.innerText = 'Remove';
+
+    bookDiv.append(bookDesc, remove);
+    booksDiv.append(bookDiv);
+    remove.addEventListener('click', () => {
+      this.removeBtnEventListener(remove, book.time);
+    });
+  };
+
+  addBookToLocalStorage = function (title, author) {
+    const d = new Date();
+    const time = d.getTime();
+    this.books.push({
+      title,
+      author,
+      time,
+    });
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.addBookToDocument({
+      title,
+      author,
+      time,
+    });
+  };
 }
 
-const method = new Methods();
-
 window.addEventListener('load', () => {
+  const method = new Methods();
+
   method.init();
 
   submitButton.addEventListener('click', (e) => {
