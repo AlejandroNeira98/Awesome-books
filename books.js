6dfc21163aa557b5f100d5a77b776c04a7a047e8
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-08 11:14:35 -0500

    Linters

--- books.js
+++ books.js
@@ -2,19 +2,18 @@ const titleInput = document.getElementById('title-input');
 const authorInput = document.getElementById('author-input');
 const submitButton = document.getElementById('submit-button');
 
-class methods {
+class Methods {
   constructor() {
-
     this.books = [];
 
     this.init = function () {
       if (JSON.parse(localStorage.getItem('books')) !== null) {
         this.books = JSON.parse(localStorage.getItem('books'));
         this.books.forEach((element) => {
-          method.addBookToDocument(element);
+          this.addBookToDocument(element);
         });
       }
-    }
+    };
 
     this.removeBook = function (time) {
       this.books = this.books.filter((book) => book.time !== time);
@@ -28,10 +27,10 @@ class methods {
 
     this.addBookToDocument = function (book) {
       const booksDiv = document.getElementById('books');
-      
+
       const bookDiv = document.createElement('div');
       bookDiv.setAttribute('data-time', book.time);
-      
+
       const bookDesc = document.createElement('p');
       bookDesc.classList.add('book-description');
       // title.classList.add('title');
@@ -56,25 +55,23 @@ class methods {
       const d = new Date();
       const time = d.getTime();
       this.books.push({
-        'title': title,
-        'author': author,
-        'time': time
+        title,
+        author,
+        time,
       });
       localStorage.setItem('books', JSON.stringify(this.books));
       this.addBookToDocument({
-        'title': title,
-        'author': author,
-        'time': time
+        title,
+        author,
+        time,
       });
     };
-
   }
 }
 
-const method = new methods;
+const method = new Methods();
 
 window.addEventListener('load', () => {
-  
   method.init();
 
   submitButton.addEventListener('click', (e) => {
@@ -85,4 +82,4 @@ window.addEventListener('load', () => {
     authorInput.value = null;
     titleInput.value = null;
   });
- });
+});
